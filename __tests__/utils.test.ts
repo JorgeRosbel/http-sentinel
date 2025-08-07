1f470e72461e7dc734706a1d0b415db64f21c4bd
rosbeldev <jorgerosbelcruzarias@gmail.com>
2025-08-07 23:36:05 +0200

    ✅ test: translate and improve http-sentinel error tests

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -12,17 +12,17 @@ describe('Utils test', () => {
     vi.clearAllMocks();
   });
 
-  it('Debe identificar los errores lanzados por http-sentinel', () => {
+  it('should identify errors thrown by http-sentinel', () => {
     try {
       const mockStatus = 404;
       tools.resolveHttpError(mockStatus);
     } catch (err: unknown) {
       expect(tools.matches(err)).toBe(true);
-      expect(matchesSpy).toBeCalledWith(err);
+      expect(matchesSpy).toHaveBeenCalledWith(err);
     }
   });
 
-  it('Debe lanzar un error especifico segun el status code', () => {
+  it('should throw a specific error according to the status code', () => {
     const httpErrorStatusCodes = [
       400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418,
       421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508,
@@ -47,7 +47,7 @@ describe('Utils test', () => {
     });
   });
 
-  it('Debe identificar el error al comprarlo con una clase', () => {
+  it('should identify the error when compared with a class', () => {
     try {
       const mockStatus = 400;
       tools.resolveHttpError(mockStatus);
@@ -60,7 +60,7 @@ describe('Utils test', () => {
     }
   });
 
-  it('Debe lanzar error desconocido si no exise status code', () => {
+  it('should throw UnknownError if status code does not exist', () => {
     try {
       const mockStatus = undefined;
       tools.resolveHttpError(mockStatus);
@@ -73,15 +73,15 @@ describe('Utils test', () => {
     }
   });
 
-  it('Debe contener todas las funciones para los metodos http', () => {
+  it('should contain all functions for HTTP methods', () => {
     const methods = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options'];
     const stn = tools.coreRequest();
 
     expect(stn).toBeTypeOf('object');
-    Object.entries(stn).forEach((element, index) => {
-      expect(element[1]).toBeTypeOf('function');
-      expect(element[0]).toBe(methods[index]);
+    Object.entries(stn).forEach(([method, fn], index) => {
+      expect(fn).toBeTypeOf('function');
+      expect(method).toBe(methods[index]);
     });
-    expect(coreRequestSpy).toBeCalled();
+    expect(coreRequestSpy).toHaveBeenCalled();
   });
 });
